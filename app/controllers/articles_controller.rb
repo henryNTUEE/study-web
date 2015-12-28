6c18c41f9612074f8f13421db6d517a3d9f03e1f
Henry Lu <b02901051@ntu.edu.tw>
2015-12-28 08:53:12 +0000

    edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,25 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
     def create
         @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was successfully created"
            redirect_to article_path(@article) 
+        else
+            render 'new'
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params
+        )
+            flash[:notice] = "Article was successgully updated"
+            redirect_to article_path(@article)
         else
             render 'new'
         end
