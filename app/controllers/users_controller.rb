284c3c547339ed59d05b4b6bc2d8c70fb7cc1ac0
Henry Lu <b02901051@ntu.edu.tw>
2016-01-02 12:55:52 +0000

    signup to signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,9 @@ class UsersController < ApplicationController
    def create
        @user = User.new(user_params)
        if @user.save
+           session[:user_id] = @user.id
            flash[:success] = "Hi #{@user.username}, Welcome to Study Web"
-           redirect_to articles_path
+           redirect_to user_path(@user)
        else
             render 'new'
        end
