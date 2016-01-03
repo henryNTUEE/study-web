70b68c630cb0118b877f8c0e8c28a9c67c9eb3ed
Henry Lu <b02901051@ntu.edu.tw>
2016-01-03 09:31:44 +0000

    add category and link with articles

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :categories
   resources :articles
   resources :users, except: [:new]
 end
